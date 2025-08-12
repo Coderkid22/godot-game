61c6c145613921a4ac3482f968238ad36d840df3
Kushagra <spvaibhav07@gmail.com>
2025-08-11 21:08:51 -0400

    major changes
    forgor to upd lol, made changs to anim (make the player look in the direction your walking etc.) and im prototyping a touch screen control scheme

--- godot_knight_platformerV2/knight-platformer/scripts/coin.gd
+++ godot_knight_platformerV2/knight-platformer/scripts/coin.gd
@@ -7,5 +7,5 @@ var score
 func _on_body_entered(_body: Node2D) -> void: # click node, click NODE tab on left, double click
 	#wanted signal
 	queue_free()# basically deletes thing
-	score += 64
+
 	
--- godot_knight_platformerV2/knight-platformer/scripts/killzone.gd
+++ godot_knight_platformerV2/knight-platformer/scripts/killzone.gd
@@ -2,11 +2,20 @@ extends Area2D
 
 @onready var timer: Timer = $Timer # ctrl hold for auto path/var to node 
 
-func _on_body_entered(_body: Node2D) -> void: # signal from node 
+func _on_body_entered(body: Node2D) -> void: # signal from node 
 	# (click node goto node tab double clikc to connect)
+	
+	Engine.time_scale = 0.67
+	body.get_node("CollisionShape2D").queue_free()
+
+	
 	timer.start()
 
 
 
+
 func _on_timer_timeout() -> void: #signal
+	
+	Engine.time_scale = 1
+	
 	get_tree().reload_current_scene()# resets game
